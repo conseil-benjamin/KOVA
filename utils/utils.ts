ea86fdf60b747083d9d15fc57c18b1027efc7b98
conseil-benjamin <benlenormand61@gmail.com>
2026-08-14 15:15:21 +0200

    update somethiong

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,4 @@
 export function getApiUrl(): string {
-    if (typeof window !== "undefined") {
-        return `http://${window.location.hostname}:3333`;
-    }
     return process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333";
 }
 
@@ -27,8 +24,5 @@ export function goBack(router: { push: (href: string) => void }): void {
 }
 
 export function getWsUrl(): string {
-    if (typeof window !== "undefined") {
-        return `http://${window.location.hostname}:3333`;
-    }
-    return "http://localhost:3333";
+    return process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3333";
 }
